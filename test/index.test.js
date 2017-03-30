6ac9eb745cedcbff984c2a25fc58045dc54e9ddb
ding <playdingnow@gmail.com>
2017-03-30 16:23:27 +0800

    test: config test case

--- test/index.test.js
+++ test/index.test.js
@@ -13,18 +13,25 @@ test.before(() => {
 
 test('should load mongo db', (t) => {
   const config = envConfig();
-  t.is(config.mongo.db, 'mongo.db1');
   t.is(Object.keys(config).length, 1);
+  t.is(config.mongo.db, 'mongo.db1');
 });
 
 test('should load mongo db with another separator', (t) => {
   const config = envConfig({ separator: '__' });
-  t.is(config.mongo.db, 'mongo.db2');
   t.is(Object.keys(config).length, 1);
+  t.is(config.mongo.db, 'mongo.db2');
 });
 
 test('should load mongo db with another prefix', (t) => {
   const config = envConfig({ prefix: 'ACONFIG' });
-  t.is(config.mongo.db, 'mongo.db3');
   t.is(Object.keys(config).length, 1);
+  t.is(config.mongo.db, 'mongo.db3');
+});
+
+test('should load mongo db with pre define config', (t) => {
+  const config = envConfig({ config: { foo: 'bar' } });
+  t.is(Object.keys(config).length, 2);
+  t.is(config.mongo.db, 'mongo.db1');
+  t.is(config.foo, 'bar');
 });
