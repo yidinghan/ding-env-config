69dbfa539064934bd86165c8daa86cb891de0e3b
ding <playdingnow@gmail.com>
2017-03-30 15:35:57 +0800

    feat: init packages from lodash

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const _ = {
-  set: require('lodash.set'),
-  defaults: require('lodash.defaults'),
-};
+const set = require('lodash.set');
+const defaults = require('lodash.defaults');
+const zipObject = require('lodash.zipobject');
 
 // once there are somethings like `CONFIG_mongo_db` in env,
 // the codes below will turn `config.kafkaConsumer.handlerConcurrency`
@@ -13,7 +12,7 @@ const _ = {
  * @returns {Object}
  */
 module.exports = (option = {}) => {
-  const opt = _.defaults(option, {
+  const opt = defaults(option, {
     setting: {},
     prefix: 'CONFIG',
     separator: '_',
@@ -21,18 +20,19 @@ module.exports = (option = {}) => {
   const { setting, separator } = opt;
   const separatorRe = new RegExp(separator, 'g');
 
-  _.each(process.env, (val, key) => {
+  Object.keys(process.env).forEach((key) => {
+    const val = process.env[key];
     if (!key.startsWith(opt.prefix)) { return; }
 
     const prefixLen = opt.prefix.length + separator.length;
     const pathWithType = key.slice(prefixLen).split(`${separator}${separator}`);
-    const envOpt = _.zipObject(['keyPath', 'valType'], pathWithType);
+    const envOpt = zipObject(['keyPath', 'valType'], pathWithType);
     if (!envOpt.keyPath) { return; }
 
     envOpt.keyPath = envOpt.keyPath.replace(separatorRe, '.');
     // every val should be string as default
     envOpt.finalVal = envOpt.valType === 'number' ? Number(val) : val;
-    _.set(setting, envOpt.keyPath, envOpt.finalVal);
+    set(setting, envOpt.keyPath, envOpt.finalVal);
   });
 
   return setting;
