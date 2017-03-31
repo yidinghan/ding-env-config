fc77af12aabbed8d643eb166d4c4f99a37ed1f0b
ding <playdingnow@gmail.com>
2017-03-31 09:29:33 +0800

    doc: about input arguments

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ const types = {
   number: Number,
 };
 
-// once there are somethings like `CONFIG_mongo_db` in env,
-// the codes below will turn `config.kafkaConsumer.handlerConcurrency`
-// to the env val and val will be number
 /**
- * envLoader
- *
- * @param {Object} option.config - description
- * @returns {Object}
- */
+ * once there are somethings like `CONFIG_mongo_db` in env,
+ * the codes below will turn `config.kafkaConsumer.handlerConcurrency`
+ * to the env val and val will be number
+ * @param {object} [payload] - input arguments
+ * @param {object} [payload.config={}] - default config object
+ * @param {string} [payload.separator=_] - symbol between key path and prefix
+ * @param {string} [payload.prefix=CONFIG] - prefix to match target environment
+ * @returns {object} parse out config
+ **/
 module.exports = (payload = {}) => {
   const { config = {}, separator = '_', prefix = 'CONFIG' } = payload;
   const separatorRe = new RegExp(separator, 'g');
